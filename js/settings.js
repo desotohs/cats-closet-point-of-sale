f45ef5d546b6723921281d43e8273c24602069ea
Zach Deibert <zachdeibert@gmail.com>
2016-09-27 16:45:11 -0500

    Added products settings page

--- js/settings.js
+++ js/settings.js
@@ -26,21 +26,33 @@ function angularCallback($scope) {
             "name": "Lightsaber",
             "desc": "Green",
             "picture": "http://www.sciencefriday.com/wp-content/uploads/2015/12/lightsaber6.jpg",
-            "price": 10
+            "price": 10,
+            "enabled": true
         },
         {
             "name": "Lightsaber",
             "desc": "Purple",
             "picture": "https://s-media-cache-ak0.pinimg.com/564x/fe/85/c2/fe85c2488c48ca28fedb31c1adbe07d9.jpg",
-            "price": 15
+            "price": 15,
+            "enabled": true
         }
     ];
     $scope.customProperties = [
         "Gender",
         "Email"
     ];
+    $scope.newProduct = {
+        "name": "",
+        "desc": "",
+        "picture": "",
+        "price": 0,
+        "enabled": true
+    };
     $scope.isCustomer = function(name) {
         return name && $scope.customers.indexOf(name) >= 0;
+    };
+    $scope.selectProduct = function(product) {
+        $scope.selectedProduct = product;
     };
     var map = {};
     for ( var i = 0; i < $scope.customers.length; ++i ) {

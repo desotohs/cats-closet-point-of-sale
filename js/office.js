6d3fac0ad001fd4420a1f9e63e0082d738fb2020
Zach Deibert <zachdeibert@gmail.com>
2016-09-27 08:36:27 -0500

    Added office page

---
+++ js/office.js
@@ -0,0 +1,17 @@
+function angularCallback($scope) {
+    $scope.customer = {
+        "name": "Obi-Wan Kenobi",
+        "balance": 100,
+        "picture": "http://vignette3.wikia.nocookie.net/swfanon/images/e/e1/Obiwankenobi_dsws.jpg/revision/latest?cb=20081204152935",
+        "properties": [
+            {
+                "name": "Gender",
+                "value": "Male"
+            },
+            {
+                "name": "Email",
+                "value": "kenobobi000@example.com"
+            }
+        ]
+    };
+}
